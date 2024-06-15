c720978e4bd2f05a679ccfada18c6290e4d56d21
dustin.yamashiro <94887665+Dustin-Yamashiro@users.noreply.github.com>
2024-06-15 15:14:14 +0900

    トップページ用のHTMLファイルをテーマファイル化

---
+++ para-log-theme/sidebar.php
@@ -0,0 +1,174 @@
+<aside class="l-sidebar u-mt--sidebar">
+    <article class="l-sidebar__item">
+        <h2 class="c-sidebarTitle">プロフィール</h2>
+        <div class="p-profile u-mt--sidebarItem-S">
+            <figure class="p-profile__img">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/profile-img.png" alt="投稿１" class="c-img">
+            </figure>
+            <section class="p-profile__desc c-desc">
+                <h2 class="p-profile__name c-desc--bold">ティンのすけ</h2>
+                <section class="p-profile__introduction">
+                    <div>
+                        <p><span class="c-desc--newLine">沖縄生まれ沖縄育ちの</span><span class="c-desc--newLine">英語喋れない系ハーフ</span></p>
+                        <span>+</span>
+                        <p><span class="c-desc--newLine">県内でITエンジニア</span><span class="c-desc--newLine">として奮闘中の新米パパ</span></p>
+                    </div>
+                    <div>
+                        <p>沖縄独自の文化である「パーラー」の魅力を発信したいと思いサイトを開設！</p>
+                        <p>各種SNSでも最新情報を発信していますので是非フォローお願いいたします！！</p>
+                    </div>
+                </section>
+            </section>
+            <section>
+                <ul class="p-profile__snsIconList p-snsIconList">
+                    <li class="c-icon c-icon--sns">
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/x-black.png" alt="x-black-logo" class="c-img"></a>
+                    </li>
+                    <li class="c-icon c-icon--sns">
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram-color.png" alt="instagram-color-logo" class="c-img"></a>
+                    </li>
+                    <li class="c-icon c-icon--sns">
+                        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-color.png" alt="facebook-color-logo" class="c-img"></a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </article>
+    <article class="l-sidebar__item">
+        <h2 class="c-sidebarTitle">人気記事ランキング</h2>
+        <ul class="p-postList p-postList--sidebar u-mt--sidebarItem-L">
+            <li class="p-postCard p-postCard--sidebar">
+                <article>
+                    <a href="#" class="p-postCard__contents">
+                        <figure class="p-postCard__thumbnail c-thumbnail">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/post-card-main/post1.png" alt="投稿１" class="c-img">
+                            <span class="p-postCard__badge c-badge c-badge--firstPlace">1</span>
+                        </figure>
+                        <section class="p-postCard__desc c-desc">
+                            <h2 class="c-desc--bold">あの有名人も学生時代に通っていた！？ 大人気老舗パーラーに行ってみた！【パーラー どんちゃん】</h2>
+                        </section>
+                    </a>
+                </article>
+            </li>
+            <li class="p-postCard p-postCard--sidebar">
+                <article>
+                    <a href="#" class="p-postCard__contents">
+                        <figure class="p-postCard__thumbnail c-thumbnail">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/post-card-main/post1.png" alt="投稿１" class="c-img">
+                            <span class="p-postCard__badge c-badge c-badge--secondPlace">2</span>
+                        </figure>
+                        <section class="p-postCard__desc c-desc">
+                            <h2 class="c-desc--bold">あの有名人も学生時代に通っていた！！？ 大人気老舗パーラーに行ってみた！！【パーラー どんちゃん】</h2>
+                        </section>
+                    </a>
+                </article>
+            </li>
+            <li class="p-postCard p-postCard--sidebar">
+                <article>
+                    <a href="#" class="p-postCard__contents">
+                        <figure class="p-postCard__thumbnail c-thumbnail">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/post-card-main/post1.png" alt="投稿１" class="c-img">
+                            <span class="p-postCard__badge c-badge c-badge--thirdPlace">3</span>
+                        </figure>
+                        <section class="p-postCard__desc c-desc">
+                            <h2 class="c-desc--bold">あの有名人も学生時代に通っていた！！？ 大人気老舗パーラーに行ってみた！！【パーラー どんちゃん】</h2>
+                        </section>
+                    </a>
+                </article>
+            </li>
+            <li class="p-postCard p-postCard--sidebar">
+                <article>
+                    <a href="#" class="p-postCard__contents">
+                        <figure class="p-postCard__thumbnail c-thumbnail">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/post-card-main/post1.png" alt="投稿１" class="c-img">
+                            <span class="p-postCard__badge c-badge c-badge--otherPlace">4</span>
+                        </figure>
+                        <section class="p-postCard__desc c-desc">
+                            <h2 class="c-desc--bold">あの有名人も学生時代に通っていた！！？ 大人気老舗パーラーに行ってみた！！【パーラー どんちゃん】</h2>
+                        </section>
+                    </a>
+                </article>
+            </li>
+            <li class="p-postCard p-postCard--sidebar">
+                <article>
+                    <a href="#" class="p-postCard__contents">
+                        <figure class="p-postCard__thumbnail c-thumbnail">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/post-card-main/post1.png" alt="投稿１" class="c-img">
+                            <span class="p-postCard__badge c-badge c-badge--otherPlace">5</span>
+                        </figure>
+                        <section class="p-postCard__desc c-desc">
+                            <h2 class="c-desc--bold">あの有名人も学生時代に通っていた！！？ 大人気老舗パーラーに行ってみた！！【パーラー どんちゃん】</h2>
+                        </section>
+                    </a>
+                </article>
+            </li>
+        </ul>
+    </article>
+    <article class="l-sidebar__item">
+        <h2 class="c-sidebarTitle">タグ一覧</h2>
+        <div class="u-mt--sidebarItem-M">
+            <ul class="p-tagList">
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">駐車場有り</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">お酒有り</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">定食屋</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">800円以下</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">お持ち帰り可能</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">夜営業有り</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">予約可能</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">団体可能</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">そば以外メニュー有り</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">年中無休</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">子供メニュー有り</span>
+                    </a>
+                </li>
+                <li class="c-tag">
+                    <a href="#" class="c-tag--contents">
+                        <span class="c-tag--text">子供席有り</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </article>
+</aside>
