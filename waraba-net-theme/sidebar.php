bd35ec05208d519d264b12650ce1c8aa46a887bf
dustin.yamashiro <94887665+Dustin-Yamashiro@users.noreply.github.com>
2024-06-30 01:57:23 +0900

    構造タグの修正

--- waraba-net-theme/sidebar.php
+++ waraba-net-theme/sidebar.php
@@ -1,13 +1,13 @@
 <aside class="l-sidebar u-mt--sidebar">
     <article class="l-sidebar__item">
         <h2 class="c-sidebarTitle">プロフィール</h2>
-        <div class="p-profile u-mt--sidebarItem-S">
+        <section class="p-profile u-mt--sidebarItem-S">
             <figure class="p-profile__img">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/profile-img.png" alt="投稿１" class="c-img">
             </figure>
             <section class="p-profile__desc c-desc">
                 <h2 class="p-profile__name c-desc--bold">ティンのすけ</h2>
-                <section class="p-profile__introduction">
+                <div class="p-profile__introduction">
                     <div>
                         <p><span class="c-desc--newLine">沖縄生まれ沖縄育ちの</span><span class="c-desc--newLine">英語喋れない系ハーフ</span></p>
                         <span>+</span>
@@ -17,7 +17,7 @@
                         <p>沖縄独自の文化である「パーラー」の魅力を発信したいと思いサイトを開設！</p>
                         <p>各種SNSでも最新情報を発信していますので是非フォローお願いいたします！！</p>
                     </div>
-                </section>
+                </div>
             </section>
             <section>
                 <ul class="p-profile__snsIconList p-snsIconList">
@@ -32,7 +32,7 @@
                     </li>
                 </ul>
             </section>
-        </div>
+        </section>
     </article>
     <article class="l-sidebar__item">
         <h2 class="c-sidebarTitle">人気記事ランキング</h2>
@@ -50,8 +50,8 @@
             <?php if ( $sidebar_posts->have_posts() ) : ?>
                 <?php while ( $sidebar_posts->have_posts() ) : ?>
                     <?php $sidebar_posts->the_post(); ?>
-                    <li class="p-postCard p-postCard--sidebar">
-                        <article>
+                    <section>
+                        <li class="p-postCard p-postCard--sidebar">
                             <a href="<?php the_permalink(); ?>" class="p-postCard__contents">
                                 <figure class="p-postCard__thumbnail c-thumbnail">
                                     <?php the_post_thumbnail( 'full', array( 'class' => 'c-img' ) ); ?>
@@ -75,12 +75,12 @@
                                     ?>
                                     <span class="p-postCard__badge c-badge <?= "c-badge--$badgeClass" ?>"><?= $rankNumber ?></span>
                                 </figure>
-                                <section class="p-postCard__desc c-desc">
+                                <div class="p-postCard__desc c-desc">
                                     <h3><?php the_title(); ?></h3>
-                                </section>
+                                </div>
                             </a>
-                        </article>
-                    </li>
+                        </li>
+                    </section>
                 <?php endwhile; ?>
                 <?php wp_reset_query(); ?>
             <?php endif; ?>
@@ -88,7 +88,7 @@
     </article>
     <article class="l-sidebar__item">
         <h2 class="c-sidebarTitle">タグ一覧</h2>
-        <div class="u-mt--sidebarItem-M">
+        <section class="u-mt--sidebarItem-M">
             <ul class="p-tagList">
                 <?php
                 $allTags = get_tags(
@@ -106,6 +106,6 @@
                     </li>
                 <?php endforeach; ?>
             </ul>
-        </div>
+        </section>
     </article>
 </aside>
