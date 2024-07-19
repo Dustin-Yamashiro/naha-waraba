820fe7607f9caa1ee660d4c362324761228fe8e0
dustin.yamashiro <94887665+Dustin-Yamashiro@users.noreply.github.com>
2024-07-19 10:20:54 +0900

    サイドメニュー用表示用のクラス名を変更

--- waraba-net-theme/index.js
+++ waraba-net-theme/index.js
@@ -33,29 +33,30 @@ const swiperMainPost = new Swiper('#swiper-mainPost', {
 
 
 // 画面スクロールを制御する関数
-const scrollStop = (status) => {
+const scrollStop = (status) =>
+{
     const body = document.getElementsByTagName('body')[0];
     body.style.overflow = status ? 'hidden' : '';
 }
 
 // サイドメニューの表示を管理する関数
-const diplaySlideMenu = () => {
+const diplaySlideMenu = () =>
+{
     const sideMenuClassList = document.getElementById('slideMenu').classList;
     const burgerButton = document.getElementById('burgerButton').classList;
     const blackFullScreen = document.getElementById('blackFullScreen').classList;
 
-    if (!sideMenuClassList.contains('p-slideMenu--action')) {
-        sideMenuClassList.add('p-slideMenu--action');
+    if (!sideMenuClassList.contains('p-slideMenu--active')) {
+        sideMenuClassList.add('p-slideMenu--active');
         burgerButton.add('p-header__burger--active');
         blackFullScreen.add('c-blackFullScreen--active');
         scrollStop(true);
         return;
     }
 
-    sideMenuClassList.remove('p-slideMenu--action');
+    sideMenuClassList.remove('p-slideMenu--active');
     burgerButton.remove('p-header__burger--active');
     blackFullScreen.remove('c-blackFullScreen--active');
     scrollStop(false);
 }
 document.getElementById('burgerButton').addEventListener('click', diplaySlideMenu);
-document.getElementById('blackFullScreen').addEventListener('click', diplaySlideMenu);
