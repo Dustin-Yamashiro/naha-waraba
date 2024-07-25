c35f5c6f2a2c6f58a32633bbb4e4ba018b82585d
dustin.yamashiro <94887665+Dustin-Yamashiro@users.noreply.github.com>
2024-07-25 13:17:12 +0900

    投稿ページの作成

---
+++ naha-waraba-theme/single.php
@@ -0,0 +1,164 @@
+<?php get_header(); ?>
+<div class="l-contents u-mt--contents-post">
+    <main class="l-main">
+        <article>
+            <header class="p-postTitle">
+                <div class="p-postTitle__subItem">
+                    <span class="p-postTitle__date c-postSubItem--colorLight">2024年7月24日</span>
+                    <a href="#" class="p-postTitle__badge c-badge c-badge--category">定番スポット</a>
+                </div>
+                <h1 class="p-postTitle__text">ミニチュアと最新映像が生み出す幻想的な空間「Little Universe OKINAWA」に行ってきた！</h1>
+            </header>
+            <section class="p-postContents u-mt--postContents-S">
+                <figure class="p-postContents__img">
+                    <img src="<?= get_template_directory_uri(); ?>/img/test-img/1.jpeg" class="c-img">
+                </figure>
+                <div class="p-postContents__text u-mt--postContents-S">
+                    <p>どうも！てぃじむなーです！</p>
+                    <p>今回は、沖縄イーアス豊崎内にある今話題の<strong class="p-postContents__text--red">「Little Universe OKINAWA」</strong>に行ってきました！最新映像技術とミニチュア達が織り出す、幻想的な空間が広がった館内は圧巻の一言。</p>
+                    <p>家族みんなで圧巻された様子をブログにまとめていますので、是非ご覧下さい！</p>
+                </div>
+            </section>
+            <section class="p-outline u-mt--postContents-M">
+                <span class="p-outline__title">目次</span>
+                <ol class="p-outline__list p-outline__list--parent">
+                    <li class="p-outline__item">
+                        <a href="#section-1">Little Universe OKINAWAとは？</a>
+                    </li>
+                    <li class="p-outline__item">
+                        <a href="#section-2">入場チケット料金</a>
+                    </li>
+                    <li class="p-outline__item">
+                        <a href="#section-3">エリア紹介</a>
+                        <ul class="p-outline__list p-outline__list--child">
+                            <li class="p-outline__item">
+                                <a href="#section-3-1">∞トンネル</a>
+                            </li>
+                            <li class="p-outline__item">
+                                <a href="#section-3-2">かがみのこみち</a>
+                            </li>
+                            <li class="p-outline__item">
+                                <a href="#section-3-3">琉球王朝</a>
+                            </li>
+                            <li class="p-outline__item">
+                                <a href="#section-3-4">レトロ東京</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="p-outline__item">
+                        <a href="#section-4">まとめ、店舗情報</a>
+                    </li>
+                </ol>
+            </section>
+            <section id="section-1" class="p-postContents u-mt--postContents-L">
+                <h2 class="p-postContents__title">「Little Universe OKINAWA」とは？</h2>
+                <div class="p-postContents__text u-mt--postContents-S">
+                    <p>Little Universe OKINAWAとは、デジタル空間演出やミニチュア、ARなどの最新テクノロジーが融合した新感覚のハイブリッド型エンタメ施設です。</p>
+                    <p>最先端技術を通して、今までにない様々な角度から沖縄を体験できる施設となっています。</p>
+                </div>
+                <figure class="p-postContents__img u-mt--postContents-S">
+                    <img src="<?= get_template_directory_uri(); ?>/img/test-img/6.jpeg" class="c-img">
+                </figure>
+            </section>
+            <section id="section-2" class="p-postContents u-mt--postContents-L">
+                <h2 class="p-postContents__title">入場チケット料金</h2>
+                <figure class="p-postContents__img u-mt--postContents-S">
+                    <img src="<?= get_template_directory_uri(); ?>/img/test-img/7.jpeg" class="c-img">
+                </figure>
+                <div class="p-postContents__text u-mt--postContents-S">
+                    <p>入場チケットの料金は上記画像の通りで、自分は県民割を利用してチケットを購入いたしました。※県民割の利用には身分証明証の提示が必要です。</p>
+                    <p>また現在、WEBを見た方限定で限定割引キャンペーンがあるようです！</p>
+                </div>
+                <figure class="p-postContents__img u-mt--postContents-S">
+                    <img src="<?= get_template_directory_uri(); ?>/img/test-img/8.jpeg" class="c-img">
+                </figure>
+            </section>
+            <section id="section-3" class="p-postContents u-mt--postContents-L">
+                <h2 class="p-postContents__title">エリア紹介</h2>
+                <div class="p-postContents__text u-mt--postContents-S">
+                    <p>Little Universe OKINAWAには最新映像技術体験とミニチュアの展示以外にも様々な魅力的なエリアがありましたのでご紹介いたします。</p>
+                </div>
+                <section id="section-3-1" class="u-mt--postContents-M">
+                    <h3 class="p-postContents__subTitle">∞トンネル</h3>
+                    <figure class="p-postContents__img u-mt--postContents-S">
+                        <img src="<?= get_template_directory_uri(); ?>/img/test-img/9.jpeg" class="c-img">
+                    </figure>
+                    <div class="p-postContents__text u-mt--postContents-S">
+                        <p>まずは入場してすぐにある「∞トンネル」からスタートです。</p>
+                        <p>進むにつれて、だんだん天井が低くなっていくトンネル進むと、まるでミニチュアの世界に行くために自身が縮んでいくような気分が味わえます。</p>
+                    </div>
+                </section>
+                <section id="section-3-2" class="u-mt--postContents-M">
+                    <h3 class="p-postContents__subTitle">かがみのこみち</h3>
+                    <figure class="p-postContents__img u-mt--postContents-S">
+                        <img src="<?= get_template_directory_uri(); ?>/img/test-img/4.jpeg" class="c-img">
+                    </figure>
+                    <div class="p-postContents__text u-mt--postContents-S">
+                        <p>トンネルを抜けた先に現れるのが一面鏡で覆われた「かがみのこみち」エリア</p>
+                        <p>迷路のようになっており、光と鏡が合わさって異世界空間のような体験ができます。</p>
+                    </div>
+                </section>
+                <section id="section-3-3" class="u-mt--postContents-M">
+                    <h3 class="p-postContents__subTitle">琉球王朝</h3>
+                    <figure class="p-postContents__img u-mt--postContents-S">
+                        <img src="<?= get_template_directory_uri(); ?>/img/test-img/10.jpeg" class="c-img">
+                    </figure>
+                    <div class="p-postContents__text u-mt--postContents-S">
+                        <p>次に現れるのが、18世紀の琉球王朝がミニチュアで再現された「琉球王朝」エリア</p>
+                        <p>首里城や守礼門といった今でも残る人気スポットなどがあり、当時の琉球の雰囲気を味わえます。</p>
+                    </div>
+                </section>
+                <section id="section-3-4" class="u-mt--postContents-M">
+                    <h3 class="p-postContents__subTitle">レトロ東京</h3>
+                    <figure class="p-postContents__img u-mt--postContents-S">
+                        <img src="<?= get_template_directory_uri(); ?>/img/test-img/11.jpeg" class="c-img">
+                    </figure>
+                    <div class="p-postContents__text u-mt--postContents-S">
+                        <p>「琉球王朝」エリアと続けて現れるのが、1980年の東京を再現した「レトロ東京」エリア</p>
+                        <p>まるで昭和の時代にタイムスリップしたような気分を味わえます。</p>
+                    </div>
+                </section>
+            </section>
+            <section id="section-4" class="p-postContents u-mt--postContents-L">
+                <h2 class="p-postContents__title">まとめ、店舗情報</h2>
+                <figure class="p-postContents__img u-mt--postContents-S">
+                    <img src="<?= get_template_directory_uri(); ?>/img/test-img/2.jpeg" class="c-img">
+                </figure>
+                <div class="p-postContents__text u-mt--postContents-S">
+                    <p>いかがでしたか？ミニチュア×ARの世界にデジタル空間演出が追加され、沖縄を新視点から楽しめる近未来的なテーマパーク「Little Universe OKINAWA」。</p>
+                    <p>みなさまもイーアス沖縄にお越しの際は、ぜひ足を運んでみて下さい！</p>
+                </div>
+                <section class="p-facilityInfo u-mt--postContents-L">
+                    <span class="p-facilityInfo__name">Little Universe OKINAWA</span>
+                    <figure class="p-facilityInfo__img">
+                        <img src="<?= get_template_directory_uri(); ?>/img/test-img/6.jpeg" class="c-img">
+                    </figure>
+                    <table class="p-facilityInfo__table">
+                        <tr>
+                            <th>住所</td>
+                            <td>〒901-0225 沖縄県豊見城市豊崎３−５３</td>
+                        </tr>
+                        <tr>
+                            <th>電話番号</td>
+                            <td>0570-098-333</td>
+                        </tr>
+                        <tr>
+                            <th>営業時間</td>
+                            <td>平日 11:00～20:00 土日祝 10:00～20:00<br>※最終入場は営業時間終了の60分前。</td>
+                        </tr>
+                        <tr>
+                            <th>定休日</td>
+                            <td>年中無休</td>
+                        </tr>
+                        <tr>
+                            <th>ホームページ</td>
+                            <td><a href="https://www.little-universe.com/" target="_blank">https://www.little-universe.com/</a></td>
+                        </tr>
+                    </table>
+                </section>
+            </section>
+        </article>
+    </main>
+    <?php get_sidebar(); ?>
+</div>
+<?php get_footer(); ?>
