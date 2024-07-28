9d03176e3c93ea3ac8190aa332e4ad65199a93f1
dustin.yamashiro <94887665+Dustin-Yamashiro@users.noreply.github.com>
2024-07-28 18:38:07 +0900

    全ファイルの削除

--- naha-waraba-theme/searchform.php
+++
@@ -1,7 +0,0 @@
-<div id="siteSearchForm" class="p-searchForm">
-    <form action="<?= esc_url( home_url('/') ); ?>" method="get" class="p-searchForm__form">
-        <input type="text" name="s" placeholder="キーワードを入力" class="p-searchForm__input">
-        <button type="submit" class="p-searchForm__submit c-icon c-icon--search"></button>
-    </form>
-    <span id="siteSearchFormClose" class="p-searchForm__close c-icon c-icon--cancel">閉じる</span>
-</div>
